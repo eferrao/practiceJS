f965ee3e4391ced38418656e65bfed30e87bbe40
Cyril Ferrao <cyrilferrao@Cyrils-MacBook-Air.local>
2015-12-24 07:23:45 -0500

    first commit

---
+++ countToTen.js
@@ -0,0 +1,12 @@
+import math 
+
+var total = 0, count = 1; 
+while (count<=5){
+	total += count; 
+	count++ 
+}
+print("Total is "+ total)
+
+//OR
+
+print(sum(range(1,10)))
---
+++ insertionsort.js
@@ -0,0 +1,10 @@
+array = [3,4,1,2,7,9];
+print(array);
+for (j = 2, array){
+	j = pointer; 
+	i = j-1;
+	while (array[j]<array[i], i>0){
+		array[i] == array[j]; 
+		i ++;
+	}
+}
