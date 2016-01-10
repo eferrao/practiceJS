2d13a3e7a5bd256b5c8be91742c2b7221ef6b0f8
Cyril Ferrao <cyrilferrao@Cyrils-MacBook-Air.local>
2016-01-10 08:14:26 -0500

    ah! changed the original. abort abort

--- insertionsort.js
+++ insertionsort.js
@@ -1,18 +1,27 @@
 //Insertion Sort 
-var array_m = new Array(31, 41, 59, 26, 37, 58);
+var array_m = new Array(31, 41, 59, 26, 41, 58);
 
 function insertionSort(array){
 
 	for (var j=1; j<array.length; j++){
 		key = array[j];
 		var i = j-1; 
-		while (array[i]<key & i>=0){
+		console.log("key: "+key);
+		console.log("array[j]: "+array[j]);
+		console.log("array[i]: "+array[i]);
+		console.log("i: "+i);
+		while (array[i]>key & i>=0){
+			console.log("arrary of i:" + array[i])
+			console.log("arrary of j:" + array[j])
 			array[i+1] = array[i];
 			i = i-1;
+			console.log(array)
+			console.log("----")
+
 		}
+		console.log("-COMPLETED ONE CHANGE-")
 		array[i+1] = key;
 	}
-	console.log(array)
 }
 
 insertionSort(array_m)
