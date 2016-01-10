440382e3be2b4a1a5ce0f7c979d34c0fd6add6b3
Cyril Ferrao <cyrilferrao@Cyrils-MacBook-Air.local>
2016-01-10 08:22:01 -0500

    changed formatting

--- Descending-insertionsort.js
+++ Descending-insertionsort.js
@@ -2,14 +2,15 @@
 var array_m = new Array(31, 41, 59, 26, 37, 58);
 
 function insertionSort(array){
-
 	for (var j=1; j<array.length; j++){
 		key = array[j];
 		var i = j-1; 
+
 		while (array[i]<key & i>=0){
 			array[i+1] = array[i];
 			i = i-1;
 		}
+		
 		array[i+1] = key;
 	}
 	console.log(array)
